76ecb6a99d8d10bcd0f201480281d537d57faab7
d4ntr0n <d4ntr0n@gmail.com>
2017-09-26 13:30:54 -0400

    Add files via upload

---
+++ NastyHacks.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class NastyHacks {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tests = sc.nextInt();
+		
+		for(int i = 1; i <= tests; i++) {
+			int noAd = sc.nextInt();
+			int yesAd = sc.nextInt();
+			int adCost = sc.nextInt();
+			
+				if(yesAd - adCost > noAd) {
+					System.out.println("advertise");
+				} else if(noAd > yesAd - adCost) {
+					System.out.println("do not advertise");
+				} else {
+					System.out.println("does not matter");
+				}
+		}
+	}
+}
