52ab172cfe8b63040702050e5825c5e25313b2a6
d4ntr0n <d4ntr0n@gmail.com>
2017-11-01 08:34:52 -0400

    Create ADifferentProblem.java

---
+++ ADifferentProblem.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ADifferentProblem {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLong()) {
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        
+        long dif = Math.abs(a-b);
+        
+        System.out.println(dif);
+        }
+    }
+
+}
