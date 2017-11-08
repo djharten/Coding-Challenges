409c9b87e7074adae7aecb6042d6a7a67872bdfc
d4ntr0n <d4ntr0n@gmail.com>
2017-11-08 13:55:25 -0500

    Add files via upload

---
+++ Slatkisi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Slatkisi {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int price = sc.nextInt();
+		int bill = sc.nextInt();
+
+		int zeros = (int) Math.pow(10, bill);
+		double roundedNumber = ((price + (0.5*zeros)) / (1*zeros)) * (1*zeros);
+		
+		price = (int) (roundedNumber/zeros)*zeros;
+		
+		System.out.println(price);
+	}
+
+}
