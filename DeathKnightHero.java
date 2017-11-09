847b0ed95209584e3b4af91e192013f9d8fd5110
d4ntr0n <d4ntr0n@gmail.com>
2017-11-09 12:43:27 -0500

    Add files via upload

---
+++ DeathKnightHero.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class DeathKnightHero {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int battles = sc.nextInt();
+		sc.nextLine();
+
+		int counter = 0;
+		int battlesWon = 0;
+		
+	//	String fight = sc.nextLine();
+
+		while (battles != counter) {
+			String fight = sc.nextLine();
+			Pattern check = Pattern.compile("CD");
+			Matcher match = check.matcher(fight);
+
+			battlesWon++;
+
+			if (match.find()) {
+				battlesWon--;
+			}
+
+			counter++;
+		}
+
+		System.out.println(battlesWon);
+	}
+}
