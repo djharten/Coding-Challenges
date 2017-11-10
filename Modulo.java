b5f8c1419dee7e386e1e6c0028bd5e795680498f
d4ntr0n <d4ntr0n@gmail.com>
2017-11-10 09:57:10 -0500

    Cleaned up
    Cleaned up code, removed a second array variable and had the first input to the array mod by 42 right away, removing the need for a second array variable.

--- Modulo.java
+++ Modulo.java
@@ -5,22 +5,19 @@ public class Modulo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int arr[] = new int[10];
-		int arrTwo[] = new int[10];
 
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-			arrTwo[i] = (arr[i] % 42);
+			arr[i] = sc.nextInt() % 42;
 		}
 
 		int counter = 10;
-		Arrays.sort(arrTwo);
+		Arrays.sort(arr);
 
 		for (int k = 1; k < arr.length; k++) {
-			if (arrTwo[k - 1] == arrTwo[k]) {
+			if (arr[k - 1] == arr[k]) {
 				counter--;
 			}
 		}
-		
 		System.out.println(counter);
 	}
 }
