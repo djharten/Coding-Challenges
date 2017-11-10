935236cc9d8e441b5e3f9f26f4f54116ef9ac00f
d4ntr0n <d4ntr0n@gmail.com>
2017-11-10 09:16:40 -0500

    Cleaned up
    Cleaned up code, input all as ints instead of String because young programmer me did not realize you could take multiple ints on the same line(ha).

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class FizzBuzz {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
+		int fizzDivisible = sc.nextInt();
+		int buzzDivisible = sc.nextInt();
+		int cases = sc.nextInt();
 		
-		String[] inputList = input.split(" ");
-		int fizzDivisible = Integer.parseInt(inputList[0]);
-		int buzzDivisible = Integer.parseInt(inputList[1]);
-		int amount = Integer.parseInt(inputList[2]);
-		
-		for(int i = 1; i <= amount; i++) {
+		for(int i = 1; i <= cases; i++) {
 			if(i % fizzDivisible == 0 && i % buzzDivisible == 0) {
 				System.out.println("FizzBuzz");
 			} else if(i % fizzDivisible == 0) {
@@ -22,5 +19,4 @@ public class FizzBuzz {
 			}
 		}
 	}
-
 }
