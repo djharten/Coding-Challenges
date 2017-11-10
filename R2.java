1b5147c6e31454d39afd3f75a68e8c31d8670f45
d4ntr0n <d4ntr0n@gmail.com>
2017-11-10 11:05:44 -0500

    Cleaned up
    Removed string input and parseInt and just used int input.

--- R2.java
+++ R2.java
@@ -4,16 +4,11 @@ public class R2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		
-		String[] inputList = input.split(" ");
-		int r1 = Integer.parseInt(inputList[0]);
-		int s = Integer.parseInt(inputList[1]);
+		int r1 = sc.nextInt();
+		int s = sc.nextInt();
 		
 		int r2 = 2*s - r1;
 		
 		System.out.println(r2);
-		
 	}
-
 }
