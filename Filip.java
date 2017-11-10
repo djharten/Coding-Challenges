a0410cf030cbbdb2a2823feee5ce9136bb0de108
d4ntr0n <d4ntr0n@gmail.com>
2017-11-10 09:13:20 -0500

    Cleaned up
    Cleaned up code, removed redundancies to make shorter(input string as the input of a StringBuilder and reversed it in one line instead of 3, eliminating the need for multiple variables to convert it).

--- Filip.java
+++ Filip.java
@@ -3,24 +3,17 @@ import java.util.Scanner;
 public class Filip {
 	public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
-		int numOne = sc.nextInt();
-		int numTwo = sc.nextInt();
-
-		String stringNumOne = Integer.toString(numOne);
-		String stringNumTwo = Integer.toString(numTwo);
 		
-		String reverseOne = new StringBuilder(stringNumOne).reverse().toString();
-		String reverseTwo = new StringBuilder(stringNumTwo).reverse().toString();
+		String numOne = new String(new StringBuilder(sc.next()).reverse());
+		String numTwo = new String(new StringBuilder(sc.next()).reverse());
 		
-		int finalOne = Integer.parseInt(reverseOne);
-		int finalTwo = Integer.parseInt(reverseTwo);
+		int finalOne = Integer.parseInt(numOne);
+		int finalTwo = Integer.parseInt(numTwo);
 		
 		if(finalOne > finalTwo) {
 			System.out.println(finalOne);
 		} else {
 			System.out.println(finalTwo);
 		}
-
 	}
-
 }
