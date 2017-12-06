3e001a059e9f50a8a0da86dd0dcba5de804ca010
d4ntr0n <d4ntr0n@gmail.com>
2017-12-06 13:21:21 -0500

    Update Skocimis.java

--- Skocimis.java
+++ Skocimis.java
@@ -3,19 +3,9 @@ import java.util.Scanner;
 public class Skocimis {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int arr[] = getNums(sc);
-		System.out.println(Math.max(arr[1]-arr[0],arr[2]-arr[1]) - 1);
-	}
-	
-	public static int[] getNums(Scanner input) {
-		int arr[] = new int[3];
-		
-		for(int i = 0; i < 3; i++) {
-			int num = input.nextInt();
-			arr[i] = num;
-		}
-		
-		return arr;
-
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int z = sc.nextInt();
+		System.out.println(Math.max(y-x,z-y) - 1);
 	}
 }
