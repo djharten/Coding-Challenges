93a711d0151fd73ce9e63f2431e5289cfd76c1b4
Daniel Hartenstine <djharten@millersville.edu>
2018-10-07 20:08:38 -0400

    Update Bishops.java

--- Java Solutions/Bishops.java
+++ Java Solutions/Bishops.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
 
 public class Bishops {
-
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 
 		while (sc.hasNextInt()) {
-			
 			int size = sc.nextInt();
 			
 			if (size == 1) {
@@ -18,5 +15,4 @@ public class Bishops {
 			}
 		}
 	}
-
 }
