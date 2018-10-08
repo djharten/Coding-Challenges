fcfc952235af5b93c7fcfb69048a964677a25cac
Daniel Hartenstine <djharten@millersville.edu>
2018-10-07 20:00:03 -0400

    Update ARealChallenge.java

--- Java Solutions/ARealChallenge.java
+++ Java Solutions/ARealChallenge.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class ARealChallenge {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double area = sc.nextDouble();
         
+        double area = sc.nextDouble();
         double length = Math.sqrt(area)*4;
         
         System.out.println(length);
     }
-
 }
