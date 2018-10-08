481e037f7a9205916f6975202b216c2aa0017dc1
Daniel Hartenstine <djharten@millersville.edu>
2018-10-07 20:08:07 -0400

    Update Bijele.java

--- Java Solutions/Bijele.java
+++ Java Solutions/Bijele.java
@@ -3,14 +3,10 @@ import java.util.Scanner;
 public class Bijele {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int king = 1 - sc.nextInt();
-		int queen = 1 - sc.nextInt();
-		int rook = 2 - sc.nextInt();
-		int bishop = 2 - sc.nextInt();
-		int knight = 2 - sc.nextInt();
-		int pawn = 8 - sc.nextInt();
-		
-		System.out.println(king + " " + queen + " " + rook + " " + bishop + " " + knight + " " + pawn);
+		int[] pieces = {1, 1, 2, 2, 2, 8};
+		for(int i = 0; i < 6; i++) {
+			int val = sc.nextInt();
+			System.out.print( (pieces[i] - val) + " ");
+		}
 	}
-
 }
